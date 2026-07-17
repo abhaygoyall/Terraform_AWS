bc6a7e0e0a5770feb0f93be8b93efd0c1e63ec70
Abhay Goyal <aby.goyal@gmail.com>
2026-07-17 13:49:57 +0530

    Security Group added

--- main.tf
+++ main.tf
@@ -4,7 +4,25 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 tags = {
     Name = "terraform-example"
+}
+user_data = <<-EOF
+              #! binbash 
+= "t2.micro" 
+echo "Hello, World" > index.xhtml 
+nohup busybox httpd -f -p 8080 & 
+EOF
+user_data_replace_on_change = true
+}
+
+resource "aws_security_group" instance {
+name = "Terraform-example-instance"
+ingress {
+    protocol = "tcp"
+    from_port = "8080"
+    to_port = "8080"
+    cidr_blocks= ["0.0.0.0/0"] 
 }
 }
